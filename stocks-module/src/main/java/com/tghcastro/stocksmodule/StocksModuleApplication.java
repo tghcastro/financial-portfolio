c0e5a841a20d01fd8aab782c77d25dca4984bbf0
Thiago de Castro <tghcastro@gmail.com>
2023-05-31 12:51:23 +0000

    Created Spring Project

---
+++ stocks-module/src/main/java/com/tghcastro/stocksmodule/StocksModuleApplication.java
@@ -0,0 +1,13 @@
+package com.tghcastro.stocksmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksModuleApplication.class, args);
+	}
+
+}
---
+++ stocks-module/src/test/java/com/tghcastro/stocksmodule/StocksModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tghcastro.stocksmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
